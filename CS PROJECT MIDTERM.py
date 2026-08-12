75da88bf009f743cb2ab19645c29d47864681958
msharinder07-alt <msharinder07@gmail.com>
2026-08-12 19:09:13 +0530

    Removed bus map

--- CS PROJECT MIDTERM.py
+++ CS PROJECT MIDTERM.py
@@ -709,12 +709,6 @@ class transportdetails:
             ctk.CTkLabel(self.inner_ui, text=tpd[1],font=("Roboto", 18,"bold")).grid(row=7,column=1,padx=5,pady=5)
             ctk.CTkLabel(self.inner_ui, text=tpd[2],font=("Roboto", 18,"bold")).grid(row=8,column=1,padx=5,pady=5)
             ctk.CTkLabel(self.inner_ui, text=tpd[3],font=("Roboto", 18,"bold")).grid(row=9,column=1,padx=5,pady=5)
-            map_widget =TkinterMapView(self.inner_ui, width=300, height=300, corner_radius=10)
-            map_widget.grid(row=10,column=0,sticky="nsew",columnspan=2)
-            map_widget.set_position(12.847197675032593, 77.6302763426442)
-            marker_1=map_widget.set_marker(12.847197675032593, 77.6302763426442, text="DPS")
-            marker_2=map_widget.set_marker(12.82991380156151, 77.6652057838779, text="House")
-            map_widget.set_zoom(10)
         if tpd[0]=="Private":
             ctk.CTkLabel(self.inner_ui,text="Mode :",font=("Roboto", 18,"bold")).grid(row=6,column=0,padx=5,pady=5)
             ctk.CTkLabel(self.inner_ui, text="Guardian :",font=("Roboto", 18,"bold")).grid(row=7,column=0,padx=5,pady=5)
